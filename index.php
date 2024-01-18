11c6ca4dc8cb5fc89fa2521b96eaf631e5dc96f2
CloudSalander <dcg070363@gmail.com>
2024-01-18 11:09:15 +0100

    What I did in 30 minutes. Stucked on the json file reading

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 /*TO-DO
--Input validation(With Exceptions)
+- Data validation of json file
+- Put file by terminal program invocation
+- With excel files too?
 */
-include('files/students.php');
 include('files/activities.php');
 
+
+function LoadStudents() : array {
+	$str = file_get_contents('./files/students.json');
+	$array = json_decode($str, true);
+	return $array;
+}
 
 function showOptions(array $activities): void {
 	foreach($activities as $key=>$activity) {
@@ -15,6 +22,8 @@ function showOptions(array $activities): void {
 function checkOptionInput(int $activities_count,string $option): bool {
 	return is_numeric($option) && $option > 0 && $option <= $activities_count;
 }
+
+var_dump(LoadStudents());
 
 showOptions($activities);
 $activity_index = readline("Hei! Quina activitat vols assignar?(Introdueix la opció numèrica)");
